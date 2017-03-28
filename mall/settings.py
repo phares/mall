18099a7d768bd03597c3d817fa0642506fb7d793
Phares <abungaphares@gmail.com>
2017-03-28 19:34:31 +0300

    db3

--- mall/settings.py
+++ mall/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'mall.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 HAYSTACK_CONNECTIONS = {
